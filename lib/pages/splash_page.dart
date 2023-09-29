c73ad03bf35022cd070436b8be49747258093ac4
Afroz pathan <afroz.p8111@gmail.com>
2023-09-29 13:40:18 +0530

    completed

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,35 @@
+// import 'dart:async';
+
+// import 'package:flutter/material.dart';
+// import 'package:to_to_list_app/pages/home_page.dart';
+
+// class SplashPage extends StatefulWidget {
+//   const SplashPage({super.key});
+
+//   @override
+//   State<SplashPage> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashPage> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Timer(const Duration(seconds: 5), () {
+//     //   Navigator.pushReplacement(
+//     //       context, MaterialPageRoute(builder: (context) => const HomePage()));
+//     // });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Image.asset(
+//           "assets/spl.jpg",
+//           height: 100,
+          
+//         ),
+//       ),
+//     );
+//   }
+// }
